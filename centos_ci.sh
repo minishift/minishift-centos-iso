d58f7adaa0dca719cfc9a63f76b3aa6007b14e89
Budh Ram Gurung <budhram.gurung01@gmail.com>
2017-02-23 20:26:25 +0530

    Issue #90 Exporting artifacts to a archive servers of CentOS CI

--- centos_ci.sh
+++ centos_ci.sh
@@ -10,7 +10,7 @@ set -e
 # that might interest this worker.
 if [ -e "jenkins-env" ]; then
   cat jenkins-env \
-    | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
+    | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId|CICO_API_KEY)=" \
     | sed 's/^/export /g' \
     > ~/.jenkins-env
   source ~/.jenkins-env
@@ -45,3 +45,23 @@ make centos_iso
 
 # Let's test with showing log enabled
 SHOW_LOG=--show-job-log make test
+
+# On reaching successfully at this point, upload artifacts
+PASS=$(echo $CICO_API_KEY | cut -d'-' -f1-2)
+
+rm -rf build/bin # Don't upload bin folder
+set +x
+# For PR build, GIT_BRANCH is set to branch name other than origin/master
+if [[ "$GIT_BRANCH" = "origin/master" ]]; then
+  # http://stackoverflow.com/a/22908437/1120530; Using --relative as --rsync-path not working
+  mkdir -p minishift-centos-iso/master/$BUILD_NUMBER/
+  cp build/* minishift-centos-iso/master/$BUILD_NUMBER/
+  RSYNC_PASSWORD=$PASS rsync -a --relative minishift-centos-iso/master/$BUILD_NUMBER/ minishift@artifacts.ci.centos.org::minishift/
+  echo "Find Artifacts here http://artifacts.ci.centos.org/minishift/minishift-centos-iso/master/$BUILD_NUMBER ."
+else
+  # http://stackoverflow.com/a/22908437/1120530; Using --relative as --rsync-path not working
+  mkdir -p minishift-centos-iso/pr/$ghprbPullId/
+  cp build/* minishift-centos-iso/pr/$ghprbPullId/
+  RSYNC_PASSWORD=$PASS rsync -a --relative minishift-centos-iso/pr/$ghprbPullId/ minishift@artifacts.ci.centos.org::minishift/
+  echo "Find Artifacts here http://artifacts.ci.centos.org/minishift/minishift-centos-iso/pr/$ghprbPullId ."
+fi
