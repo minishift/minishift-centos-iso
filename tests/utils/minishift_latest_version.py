cefeb97b970cdffab0c62599cf3c98596458c584
Budh Ram Gurung <budhram.gurung01@gmail.com>
2017-11-15 22:02:09 +0530

    Issue #176 Update tests to be run in macOS

--- tests/utils/minishift_latest_version.py
+++ tests/utils/minishift_latest_version.py
@@ -3,11 +3,15 @@
 import requests
 import re
 import os
+import platform
 
-OS = os.getenv('OS', 'linux')
+distributionInfo = platform.platform()
+
+matchObj = re.match( r'linux|darwin', distributionInfo, re.I)
+OS= matchObj.group().lower()
 
 def main():
-    pattern = re.compile(".*%s-amd64.(tgz|zip)$" % OS)
+    pattern = re.compile(".*%s-amd64.tgz$" % OS)
     r = requests.get('https://api.github.com/repos/minishift/minishift/releases/latest')
     for asset in r.json()['assets']:
         if pattern.match(asset['name']):
